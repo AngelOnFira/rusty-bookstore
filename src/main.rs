737876a4c98350242b964d790325ce9a82cb4a73
Forest Anderson <forestkzanderson@gmail.com>
2021-12-01 12:19:08 -0500

    Changed template

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,52 @@
-use yew::prelude::*;
+use anyhow::Result;
+use std::process::Command;
+use structopt::StructOpt;
+use wasm_run::prelude::*;
 
-#[derive(Debug)]
-pub struct Model;
-impl Component for Model {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        unimplemented!()
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <main>
-                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-                <h1>{ "Hello World!" }</h1>
-                <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-            </main>
-        }
-    }
+#[wasm_run::main("app", pre_build = pre_build, other_cli_commands = other_cli_commands)]
+#[derive(StructOpt, Debug)]
+enum Cli {
+    UpdateCss,
 }
 
-fn main() {
-    yew::start_app::<Model>();
+fn pre_build(args: &DefaultBuildArgs, profile: BuildProfile, command: &mut Command) -> Result<()> {
+    let package = args.frontend_package();
+
+    download_blueprint_css(package, false)?;
+
+    match profile {
+        BuildProfile::Profiling | BuildProfile::Release => {
+            command.args(&["--features", "wee_alloc"]);
+        }
+        BuildProfile::Dev => {
+            command.args(&["--features", "console_error_panic_hook"]);
+        }
+    }
+
+    Ok(())
+}
+
+fn other_cli_commands(cli: Cli, _metadata: &Metadata, package: &Package) -> Result<()> {
+    match cli {
+        Cli::UpdateCss => {
+            download_blueprint_css(package, true)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn download_blueprint_css(package: &Package, force: bool) -> Result<()> {
+    let css_path = package
+        .manifest_path
+        .parent()
+        .unwrap()
+        .join("static")
+        .join("blueprint.css");
+
+    if force || !css_path.exists() {
+        yewprint_css::download_css(&css_path)?;
+    }
+
+    Ok(())
 }
